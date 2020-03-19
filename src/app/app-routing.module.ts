f6a01db3917ae7e285adb6f0259748762e9a003f
Matthew Alner <matthewalner@googlemail.com>
2020-03-19 20:31:48 +0000

    Add homepage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: HomePageComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
