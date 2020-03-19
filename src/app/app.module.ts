82fdf2b6779bdd92ef36f1eba0db7701d867694e
Matthew Alner <matthewalner@googlemail.com>
2020-03-19 21:49:54 +0000

    Break out host card in to a component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { FooterComponent } from './home-page/footer/footer.component';
 import { NavbarComponent } from './home-page/navbar/navbar.component';
+import { HostCardComponent } from './home-page/host-card/host-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
     FooterComponent,
-    NavbarComponent
+    NavbarComponent,
+    HostCardComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/home-page/host-card/host-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-host-card',
+  templateUrl: './host-card.component.html',
+  styles: []
+})
+export class HostCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
